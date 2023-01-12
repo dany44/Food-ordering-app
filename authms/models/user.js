847aeda298621a1c5374529fa9bf6d934fdf1d4e
dany44 <danykhadhar@hotmail.fr>
2023-01-12 20:02:53 +0100

    Ordering menu added

--- authms/models/user.js
+++ authms/models/user.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
 })
 
 module.exports = mongoose.model('User', userSchema);
+
--- authms/routes/routes.js
+++ authms/routes/routes.js
@@ -1,72 +1,23 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-const User = require('../models/user');
-const mongodb = require('mongodb');
+const pool = require('../helpers/database.js');
+const referralCodes = require("referral-codes");
+require('dotenv').config()
 
-router.post('/register', async (req, res) => {
-  if (!req.body.first_name || !req.body.last_name || !req.body.email || !req.body.password) {
-    return res.status(400).send({ error: 'All fields are required.' });
-  }
-
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-  const user = new User({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    password: hashedPassword,
-  })
-
-  const result = await user.save()
-
-  const { password, ...data } = await result.toJSON()
-
-  res.send(data);
-})
-
-router.post('/login', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email })
-  if (!user) {
-    return res.status(404).send({
-      message: "user not found"
-    })
-  }
-
-  if (!await bcrypt.compare(req.body.password, user.password)) {
-    return res.status(400).send({
-      message: "invalid credentials"
-    })
-  }
-
-  const token = jwt.sign({ _id: user._id }, "secret")
-
-  res.cookie('jwt', token, {
-    httpOnly: false,
-    maxAge: 24 * 60 * 60 * 1000 // 1 day
-  })
-
-  res.send({
-    message: 'success',
-    token: token,
-    first_name: user.first_name,
-    last_name: user.last_name
-  });
-})
 
 router.get('/user', async (req, res) => {
   try {
-    const cookie = req.headers.authorization.split(' ')[1];
-    const claims = jwt.verify(cookie, 'secret')
+    const token = req.header('auth-token');
+    // check if the token is valid
+    if (!token) return res.status(401).json({ message: "Access denied" });
 
-    if (!claims) {
-      return res.status(401).send({
-        message: "unauthenticated"
-      })
-    }
+    const user_id = jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) return res.status(401).json({ message: "Access denied" });
+    });
 
-    const user = await User.findOne({ _id: claims._id })
+    const checkQuery = `SELECT * FROM user WHERE iduser = ?`;
+    const user = await pool.query(checkQuery, [user_id]);
 
     const { password, ...data } = await user.toJSON()
 
@@ -78,105 +29,147 @@ router.get('/user', async (req, res) => {
   }
 })
 
-router.post('/logout', (req, res) => {
-  res.cookie('jwt', '', { maxAge: 0 })
-
-  res.send({
-    message: 'success'
-  })
-})
-
-router.post('/delete_account', async (req, res) => {
+router.post("/tokenCheckup", async (req, res) => {
   try {
-    // First, get the password from the request body
-    const password = req.body.password;
+    // check jwt token
 
-    // Then, get the JWT from the request headers
-    const token = req.body.token
-    const claims = jwt.verify(token, 'secret');
-    console.log("log1: " + token);
-    console.log("log2: " + claims._id)
-    if (!claims) {
-      return res.status(401).send({
-        message: 'unauthenticated2'
-      });
-    }
-    // Find the user with the matching ID and password
-    const _id = mongodb.ObjectId(claims._id);
+    const token = req.header('auth-token');
+    console.log(token);
 
-    const user = await User.findOne({ _id })
+    // check if the token is valid
+    if (!token) return res.status(401).json({ message: "Access denied" });
 
-    if (user && await bcrypt.compare(user.password, password)) {
-      // If no user was found, send a response with a status code of 401 (Unauthorized)
-      return res.status(401).send({
-        message: 'incorrect password'
-      });
-    }
-
-    // If the user was found, delete the user document from the collection
-    await User.deleteOne({ _id: claims._id });
-
-    // Send a response with a status code of 200 (OK)
-    res.sendStatus(200);
-  } catch (error) {
-    return res.status(401).send({
-      message: 'unauthenticated3'
+    jwt.verify(token, 'secret', (err, decoded) => {
+      if (err) return res.status(401).json({ message: "Access denied" });
     });
+    console.log("Access granted");
+    res.status(200).json({ message: "Access granted" });
+  }
+  catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
-router.post('/edit_account', async (req, res) => {
+router.post('/register', async (req, res) => {
+
   try {
-    // First, get the password from the request body
+    const { first_name, last_name, username, password, email, referal_by, role } = req.body;
 
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const token = req.body.token
-
-    const claims = jwt.verify(token, 'secret');
-    if (!claims) {
-      return res.status(401).send({
-        message: 'unauthenticated1'
-      });
+    // check for fields required 
+    if (!first_name || !last_name || !email || !password || !username || !role) {
+      return res.status(400).send({ error: 'Fields are required.' });
     }
-    // Find the user with the matching ID and password
-    const _id = mongodb.ObjectId(claims._id);
-    const user = await User.findOne({ _id })
 
-    if (!user) {
+    // generate a unique referal number
+    let referal_code = referralCodes.generate({
+      pattern: '#####-##-###'
+    });
+
+    // get first element of the array
+    referal_code = referal_code[0];
+
+    // check if the user already exists
+    const checkUser = `SELECT * FROM user WHERE username = ?`;
+    const checkUserResult = await pool.query(checkUser, [username])
+    if (checkUserResult.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // get actual datetime in format YYYY-MM-DD HH:MM:SS
+    const date = new Date();
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // insert the user
+    const sqlQuery = `INSERT INTO user (first_name, last_name, username,
+       password, email, referal_by, referal_code, role, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    await pool.query(sqlQuery, [first_name, last_name, username, hashedPassword, email, referal_by, referal_code, role, date, date]);
+    res.status(200).json({ message: "User created successfully" });
+
+  } catch (error) {
+    return res.status(401).send({
+      message: 'Sign up failed'
+    });
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // check if the user exists
+    const checkUser = `SELECT * FROM user WHERE username = ?`;
+    const checkUserResult = await pool.query(checkUser, [username]);
+    if (checkUserResult.length == 0) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+
+    // check if the password is correct
+    const validPassword = await bcrypt.compare(password, checkUserResult[0].password);
+    if (!validPassword) return res.status(400).json({ message: "Invalid password" });
+
+    // create and assign a token
+    const token = jwt.sign({ id: checkUserResult[0].iduser }, process.env.SECRET, { expiresIn: '2h' });
+
+    res.header('auth-token', token).json({ token: token, username: username, role: checkUserResult[0].role });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+})
+
+router.post('/delete', async (req, res) => {
+
+  try {
+    const token = req.header('auth-token');
+    const password = req.body.password;
+
+    // check if the token is valid
+    if (!token) return res.status(401).json({ message: "Access denied" });
+
+    const user_id = jwt.verify(token, process.env.SECRET);
+
+    const checkQuery = `SELECT password FROM user WHERE iduser = ?`;
+    const pass = await pool.query(checkQuery, [user_id.id]);
+
+    if (await bcrypt.compare(password, pass[0].password) != true) {
       // If no user was found, send a response with a status code of 401 (Unauthorized)
       return res.status(401).send({
         message: 'incorrect password'
       });
     }
+    // delete user
+    const deleteQuery = `DELETE FROM user WHERE iduser = ?`;
+    const result = await pool.query(deleteQuery, [user_id.id]);
 
-    // If the user was found, change the first_name, last_name and email if there are different 
-    if (user.first_name !== first_name) {
-      user.first_name = first_name;
-    }
-    if (user.last_name !== last_name) {
-      user.last_name = last_name;
-    }
-    if (user.email !== email) {
-      user.email = email;
-    }
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-    if (user.password !== hashedPassword) {
-      user.password = hashedPassword;
-    }
+    res.status(200).json({message: "success !"});
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-    // Save the updated user
-    await user.save();
+router.post('/update', async (req, res) => {
+  try {
+    const { first_name, last_name, username, email } = req.body;
 
-    // Send a response with a status code of 200 (OK)
-    res.sendStatus(200);
-  } catch (error) {
-    return res.status(401).send({
-      message: 'unauthenticated2'
-    });
+    const token = req.header('auth-token');
+    // check if the token is valid
+    if (!token) return res.status(401).json({ message: "Access denied" });
+
+    const user_id = jwt.verify(token, 'secret');
+
+    // get actual datetime in format YYYY-MM-DD HH:MM:SS
+    const date = new Date();
+
+    // update user
+    const sqlQuery = `UPDATE user SET first_name = ?, last_name = ?, username = ?, email = ?, updated_at = ? WHERE iduser = ?`;
+    const result = await pool.query(sqlQuery, [first_name, last_name, username, email, date, user_id.id]);
+    res.status(200).json({message: "success !"});
+
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
